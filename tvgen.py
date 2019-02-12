ab023af2f9635bd2b2ae37345b6257e1bec58f8a
Francisco Troncoso <ftroncoso@uvigo.es>
2019-02-12 13:03:29 +0100

    Modified tvgen script to accept an additional test partition.

--- tvgen.py
+++ tvgen.py
@@ -13,20 +13,29 @@ def list_files(folder, ext):
                 yield os.path.join(dirpath, filename)
 
 
-def write_tv_files(folder, valid_percentage):
+def write_tv_files(folder, valid_percentage, test_percentage):
     filenames = list(list_files(folder, ".jpg"))
     random.shuffle(filenames)
-    n = int(len(filenames) * valid_percentage)
+    l = len(filenames)
+    nt = int(l * test_percentage)
+    nv = int(l * valid_percentage)
+    with open('test.txt', 'w') as f:
+        for file in filenames[:nt]:
+            f.write(file + '\n')
     with open('valid.txt', 'w') as f:
-        for file in filenames[:n]:
+        for file in filenames[nt:nt+nv]:
             f.write(file + '\n')
     with open('train.txt', 'w') as f:
-        for file in filenames[n:]:
+        for file in filenames[nt+nv:]:
             f.write(file + '\n')
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('Usage: {} <folder> <valid_percentage>'.format(sys.argv[0]))
+    if len(sys.argv) < 3:
+        print('Usage: {0} <folder> [valid_percentage=0] [test_percentage=0]'
+            .format(sys.argv[0]))
         exit(1)
-    write_tv_files(sys.argv[1], float(sys.argv[2]) / 100)
+    folder = sys.argv[1]
+    valid_percentage = float(sys.argv[2]) / 100 if len(sys.argv) > 2 else 0
+    test_percentage = float(sys.argv[3]) / 100 if len(sys.argv) > 3 else 0
+    write_tv_files(sys.argv[1], valid_percentage, test_percentage)
